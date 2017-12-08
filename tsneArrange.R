c6e9ee2c390b4deb3693f6547e35c67863c9283a
pearcedom <dominic.pearce@ed.ac.uk>
2017-12-08 16:48:51 +0000

    included perplexity argument

--- tsneArrange.R
+++ tsneArrange.R
@@ -1,6 +1,8 @@
 #Given a data frame/matrix, calculate tsne coordinates and arrange as a data frame
-tsneArrange <- function(data){
+tsneArrange <- function(data, perplexity = 30){
         library(Rtsne)
-        tsne_out <- Rtsne(data)$Y 
+        tsne_out <- Rtsne(data, 
+                          perplexity = perplexity,
+                          max_iter = 5000)$Y 
         data.frame(tsne_out, xpr_id = row.names(data))
 }
